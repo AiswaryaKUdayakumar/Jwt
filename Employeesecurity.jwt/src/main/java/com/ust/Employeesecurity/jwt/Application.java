c2a62acc637e2b75cdacf92c1b02692d64fd718e
AiswaryaKUdayakumar <aiswaryakuday@gmail.com>
2023-04-27 15:57:39 +0530

    april 27

---
+++ Employeesecurity.jwt/src/main/java/com/ust/Employeesecurity/jwt/Application.java
@@ -0,0 +1,13 @@
+package com.ust.Employeesecurity.jwt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
